5150e5df76e3d22cdfe1c27dc0befc81703d71b7
arturo <ahpareja@gmail.com>
2022-12-02 20:08:48 -0500

    Fetch api giphy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [gifs, updateGifs] = useState(['https://media0.giphy.com/media/nQsjN6yCqXwBoELeSi/giphy.gif?cid=ecf05e47qjz3v33auy4tv9bdnc4em6xv5t0if0qilv9dflh7&rid=giphy.gif&ct=g']);
+  const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=AZXFi0gvCDPNwoB1RpGg81kZckh53G4P&q=the+office&limit=25&offset=0&rating=g&lang=en';
+  const [gifs, setGifs] = useState([]);
 
+ useEffect(() => {
+  fetch(apiURL)
+  .then(res => res.json())
+  .then(response => {
+    const { data } = response;
+    const gifs = data.map(img => img.images.downsized_medium.url)
+    setGifs(gifs)
+  })
+  // setGifs(GIFS_2)
+  
+ }, [])
 
   return (
     <div className="App">
       <section className="App-content">
-         <img src={gifs} />
+        {
+          gifs.map(singleGif =>
+            <img src={singleGif} />
+          )
+        }
+        {/* <button className='btn' onClick={() => setGifs(GIFS_2)}>Change Gifs</button> */}
       </section>
     </div>
   );
